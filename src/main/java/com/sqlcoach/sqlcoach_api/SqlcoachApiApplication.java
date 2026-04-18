ff8f5c83313210ae83943fe9aaa10b634021725d
dorameme <doradorav@naver.com>
2026-04-18 20:53:23 +0900

    init: spring boot 프로젝트 초기설정

---
+++ src/main/java/com/sqlcoach/sqlcoach_api/SqlcoachApiApplication.java
@@ -0,0 +1,13 @@
+package com.sqlcoach.sqlcoach_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlcoachApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqlcoachApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sqlcoach/sqlcoach_api/SqlcoachApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sqlcoach.sqlcoach_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqlcoachApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
